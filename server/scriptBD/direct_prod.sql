6feeebd3d1a00202d2a8475a142d823a5049b92b
AldwynN <AldwynN@users.noreply.github.com>
2019-04-11 13:07:48 +0200

    Update database

--- server/scriptBD/direct_prod.sql
+++ server/scriptBD/direct_prod.sql
@@ -1,32 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 4.1.4
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Client :  127.0.0.1
--- Généré le :  Mar 02 Avril 2019 à 16:42
--- Version du serveur :  5.6.15-log
--- Version de PHP :  5.4.24
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: direct_prod
+-- ------------------------------------------------------
+-- Server version	8.0.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `direct_prod`
+-- Table structure for table `advertisements`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `advertisements`
---
-
-CREATE TABLE IF NOT EXISTS `advertisements` (
+DROP TABLE IF EXISTS `advertisements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `advertisements` (
   `idAdvertisement` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `description` text NOT NULL,
@@ -35,54 +31,84 @@ CREATE TABLE IF NOT EXISTS `advertisements` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `idUser` int(11) NOT NULL,
   PRIMARY KEY (`idAdvertisement`),
-  KEY `idUser` (`idUser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  KEY `idUser` (`idUser`),
+  CONSTRAINT `advertisements_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `advertisements`
+-- Dumping data for table `advertisements`
 --
 
-INSERT INTO `advertisements` (`idAdvertisement`, `title`, `description`, `organic`, `valid`, `date`, `idUser`) VALUES
-(1, 'Je vend mon canard', 'Godefroid mon canard a 3 ans, couleur blanc tirant sur le gris, bec jaune avec trou.', 1, 1, '2019-03-19 15:24:22', 1),
-(2, 'asd', 'asd', 1, 1, '2019-04-16 07:25:23', 4);
-
--- --------------------------------------------------------
+LOCK TABLES `advertisements` WRITE;
+/*!40000 ALTER TABLE `advertisements` DISABLE KEYS */;
+INSERT INTO `advertisements` VALUES (1,'Je vend mon canard','Godefroid mon canard a 3 ans, couleur blanc tirant sur le gris, bec jaune avec trou.',1,1,'2019-04-11 11:04:38',1);
+/*!40000 ALTER TABLE `advertisements` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `pictures`
+-- Table structure for table `pictures`
 --
 
-CREATE TABLE IF NOT EXISTS `pictures` (
+DROP TABLE IF EXISTS `pictures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `pictures` (
   `idPicture` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `type` varchar(50) NOT NULL,
   `idAdvertisement` int(11) NOT NULL,
   PRIMARY KEY (`idPicture`),
-  KEY `idAdvertisement` (`idAdvertisement`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `idAdvertisement` (`idAdvertisement`),
+  CONSTRAINT `pictures_ibfk_1` FOREIGN KEY (`idAdvertisement`) REFERENCES `advertisements` (`idAdvertisement`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `rate`
+-- Dumping data for table `pictures`
 --
 
-CREATE TABLE IF NOT EXISTS `rate` (
+LOCK TABLES `pictures` WRITE;
+/*!40000 ALTER TABLE `pictures` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pictures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rate`
+--
+
+DROP TABLE IF EXISTS `rate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `rate` (
   `rating` int(11) NOT NULL,
   `comment` text NOT NULL,
   `idUser` int(11) NOT NULL,
   `idAdvertisement` int(11) NOT NULL,
   KEY `idUser` (`idUser`,`idAdvertisement`),
-  KEY `idAdvertisement` (`idAdvertisement`)
+  KEY `idAdvertisement` (`idAdvertisement`),
+  CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`),
+  CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`idAdvertisement`) REFERENCES `advertisements` (`idAdvertisement`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `users`
+-- Dumping data for table `rate`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+LOCK TABLES `rate` WRITE;
+/*!40000 ALTER TABLE `rate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -95,40 +121,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `description` text NOT NULL,
   `salt` varchar(50) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`idUser`, `password`, `email`, `name`, `city`, `canton`, `postCode`, `streetAndNumber`, `admin`, `description`, `salt`) VALUES
-(1, '1234', 'romain.prtt@eduge.ch', 'Rorobocop', 'Confignon', 'Genève', '1232', '14, Rue Joseph-Berthet', 0, 'J''aime faire du scratch parce que grâce à ce programme je me prends pour un vrai informaticien.', '1234'),
-(3, 'test', 'khalil.mddb@eduge.ch', 'Kirua', 'asd', 'asd', 'asd', '49 Boulevard du Pont-d&#39;Arve', 0, 'testtrste', 'asd'),
-(4, '21995fd0a97ac7a99a687beb9367e80f74e34700', 'JDM@gmail.com', 'Jacot', 'Genève', 'Genève', '1205', '49 Boulevard du Pont-d&#39;Arve', 0, 'asdasdasd', '11793474565ca36585edea68.65121056');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'21995fd0a97ac7a99a687beb9367e80f74e34700','romain.prtt@eduge.ch','Rorobocop','Confignon','Genève','1232','14, Rue Joseph-Berthet',1,'J\'aime faire du scratch parce que grâce à ce programme je me prends pour un vrai informaticien.','11793474565ca36585edea68.65121056');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `advertisements`
---
-ALTER TABLE `advertisements`
-  ADD CONSTRAINT `advertisements_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`);
-
---
--- Contraintes pour la table `pictures`
---
-ALTER TABLE `pictures`
-  ADD CONSTRAINT `pictures_ibfk_1` FOREIGN KEY (`idAdvertisement`) REFERENCES `advertisements` (`idAdvertisement`);
-
---
--- Contraintes pour la table `rate`
---
-ALTER TABLE `rate`
-  ADD CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`),
-  ADD CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`idAdvertisement`) REFERENCES `advertisements` (`idAdvertisement`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-11 13:07:02
